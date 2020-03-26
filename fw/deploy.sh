903e6cd931f2a54358cdf429b867a980aeda0762
Jaroslav Jindrich <jajindri@microsoft.com>
2020-03-26 10:31:05 +0100

    subnet changes

--- fw/deploy.sh
+++ fw/deploy.sh
@@ -4,8 +4,7 @@ rg='JJDevV2-Infra'
 az group deployment create -g $rg --template-file deploy-fw.json --parameters deploy-fw.params.json
 
 az network vnet subnet update -g $rg -n DmzInfra --vnet-name JJDevV2Network --route-table jjdevv2fw-rt
-az network vnet subnet update -g $rg -n DmzWeb --vnet-name JJDevV2Network --route-table jjdevv2fw-rt
-az network vnet subnet update -g $rg -n DmzAks --vnet-name JJDevV2Network --route-table jjdevv2fw-rt
+az network vnet subnet update -g $rg -n DmzAks --vnet-name JJDevV2NetworkApp --route-table jjdevv2fw-rt
 az network vnet subnet update -g $rg -n DmzApp --vnet-name JJDevV2NetworkApp --route-table jjdevv2fw-rt
 az network vnet subnet update -g $rg -n GatewaySubnet --vnet-name JJDevV2Network --route-table jjdevv2fw-vpn-rt
 az network vnet subnet update -g $rg -n AzureApplicationGatewaySubnet --vnet-name JJDevV2Network --route-table jjdevv2fw-appgw-rt
