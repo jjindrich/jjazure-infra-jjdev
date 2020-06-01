802a051ffe4409b877a038eee5a40c0578bb9191
Jaroslav Jindrich <jajindri@microsoft.com>
2020-06-01 11:22:33 +0200

    what-if

--- appgw/deploy.sh
+++ appgw/deploy.sh
@@ -1,4 +1,5 @@
 rg='JJDevV2-Infra'
 
 # install appgw and nsg
+#az deployment group what-if -g $rg --template-file deploy-appgw.json --parameters deploy-appgw.params.json
 az deployment group create -g $rg --template-file deploy-appgw.json --parameters deploy-appgw.params.json
