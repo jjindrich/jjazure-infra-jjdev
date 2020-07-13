849969eecf41a08f03ea7bbaaec16e9e272c82cd
Jaroslav Jindrich <jajindri@microsoft.com>
2020-07-13 13:37:57 +0200

    fw fix routing - separate route for SpoekApp

--- fw/deploy.sh
+++ fw/deploy.sh
@@ -3,9 +3,9 @@ rg='JJDevV2-Infra'
 # install firewall and add routes
 az deployment group create -g $rg --template-file deploy-fw.json --parameters deploy-fw.params.json
 
-az network vnet subnet update -g $rg -n DmzInfra --vnet-name JJDevV2Network --route-table jjdevv2fw-rt
-az network vnet subnet update -g $rg -n DmzAks --vnet-name JJDevV2NetworkApp --route-table jjdevv2fw-rt
-az network vnet subnet update -g $rg -n DmzApp --vnet-name JJDevV2NetworkApp --route-table jjdevv2fw-rt
-az network vnet subnet update -g $rg -n GatewaySubnet --vnet-name JJDevV2Network --route-table jjdevv2fw-vpn-rt
-az network vnet subnet update -g $rg -n AzureApplicationGatewaySubnet --vnet-name JJDevV2Network --route-table jjdevv2fw-appgw-rt
-az network vnet subnet update -g $rg -n DmzApiMngmt --vnet-name JJDevV2Network --route-table jjdevv2fw-apimngmt-rt
+az network vnet subnet update -g $rg -n DmzInfra --vnet-name JJDevV2Network --route-table jjdevv2fw-hubinfra-rt
+az network vnet subnet update -g $rg -n DmzAks --vnet-name JJDevV2NetworkApp --route-table jjdevv2fw-spokeapp-rt
+az network vnet subnet update -g $rg -n DmzApp --vnet-name JJDevV2NetworkApp --route-table jjdevv2fw-spokeapp-rt
+az network vnet subnet update -g $rg -n GatewaySubnet --vnet-name JJDevV2Network --route-table jjdevv2fw-hubvpn-rt
+az network vnet subnet update -g $rg -n AzureApplicationGatewaySubnet --vnet-name JJDevV2Network --route-table jjdevv2fw-hubappgw-rt
+az network vnet subnet update -g $rg -n DmzApiMngmt --vnet-name JJDevV2Network --route-table jjdevv2fw-hubapimngmt-rt
