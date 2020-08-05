ab7fe545f5595b2da8feded2b01fd0f8adacd31e
Jaroslav Jindrich <jajindri@microsoft.com>
2020-08-05 15:51:25 +0200

    appgw fix name

--- appgw/delete.sh
+++ appgw/delete.sh
@@ -1,3 +1,4 @@
 rg='JJDevV2-Infra'
 
 az resource delete -g $rg -n jjdevv2appgw --resource-type "Microsoft.Network/applicationGateways"
+az resource delete -g $rg -n jjdevv2appgwpolicy --resource-type "Microsoft.Network/ApplicationGatewayWebApplicationFirewallPolicies"
