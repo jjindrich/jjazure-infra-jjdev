11fd91cd93b498e4470f60243c667e5bebaa6ffb
Jaroslav Jindrich <jajindri@microsoft.com>
2020-11-18 15:33:07 +0100

    delete publicip appgw

--- appgw/delete.sh
+++ appgw/delete.sh
@@ -2,3 +2,4 @@ rg='JJDevV2-Infra'
 
 az resource delete -g $rg -n jjdevv2appgw --resource-type "Microsoft.Network/applicationGateways"
 az resource delete -g $rg -n jjdevv2appgwpolicy --resource-type "Microsoft.Network/ApplicationGatewayWebApplicationFirewallPolicies"
+az resource delete -g $rg -n jjdevv2appgw-ip --resource-type "Microsoft.Network/publicIPAddresses"
