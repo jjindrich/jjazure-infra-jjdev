6f07614ef9b0eb207a7ad866bbb1b43676551184
Jaroslav Jindrich <jajindri@microsoft.com>
2020-11-25 17:19:29 +0100

    vpn gw and er gw

--- vnet/deploy.sh
+++ vnet/deploy.sh
@@ -2,3 +2,7 @@ rg='JJDevV2-Infra'
 
 # creates virtual network wit NSGs
 az deployment group create -g $rg --template-file deploy-vnet.json --parameters deploy-vnet.params.json
+
+# deploy vpn and connections
+#az deployment group create -g $rg --template-file deploy-vpn.json --parameters deploy-vpn.params.json --parameters vpnGwConnectionKey=
+#az deployment group create -g $rg --template-file deploy-vpn.json --parameters deploy-vpn.params-er.json --parameters vpnGwConnectionKey=
