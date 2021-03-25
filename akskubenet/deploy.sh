d4df9d6d99abb12628e762469194ad96427d07fa
Jaroslav Jindrich <jajindri@microsoft.com>
2021-03-25 16:36:55 +0100

    aks kubenet

--- akskubenet/deploy.sh
+++ akskubenet/deploy.sh
@@ -10,10 +10,10 @@ az aks create -n jjakskubenet -g $rgaks \
     --node-vm-size Standard_B2s --enable-cluster-autoscaler --min-count 1 --max-count 3 \
     --enable-managed-identity \
     --network-plugin kubenet \
-    --vnet-subnet-id $(az network vnet subnet show --vnet-name JJDevV2NetworkApp -g $rg -n DmzAksPrivate --query id -o tsv) \
+    --vnet-subnet-id $(az network vnet subnet show --vnet-name JJDevV2NetworkApp -g $rg -n DmzAks --query id -o tsv) \
     --enable-aad --aad-admin-group-object-ids ee2529f7-8f82-450a-9cd5-53968fff8c5d \
     --enable-addons azure-policy,monitoring \
     --workspace-resource-id $(az monitor log-analytics workspace show -g $rgmonitor -n jjdev-analytics --query id -o tsv) 
 
-# Deploy application using intenal loadbalancer
-#kubectl apply -f sample-private.yaml
+# Deploy application using loadbalancer
+#kubectl apply -f sample-vote.yaml
