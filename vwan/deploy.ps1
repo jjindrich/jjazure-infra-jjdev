6b77f5c2fbb0ddb64d3ad41babac3fb60c0af50b
Jaroslav Jindrich <jajindri@microsoft.com>
2021-06-18 21:21:51 +0200

    vwan aks

--- vwan/deploy.ps1
+++ vwan/deploy.ps1
@@ -16,3 +16,15 @@ az deployment group create --resource-group $rg --template-file deploy.bicep --p
 #$rg = "jjvnet-rg"
 #az group create -n $rg -l westeurope
 #az deployment group create --resource-group $rg --template-file deploy-spokeonly.bicep --parameters adminPassword=Azure-1234512345
+
+# nasazeni AKS za FW
+<#
+$rg='jjvwan-rg'
+$rgaks='jjvwan-aks-rg'
+az group create -n $rgaks -l westeurope
+az aks create -n jjvwanaks -g $rgaks `
+    -x -c 2 -z 1 2 3 `
+    --enable-managed-identity `
+    --network-plugin azure `
+    --vnet-subnet-id $(az network vnet subnet show --vnet-name jjvwan-weu-app1 -g $rg -n snet-default --query id -o tsv)    
+#>
