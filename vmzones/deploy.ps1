ae59e11068da832ef0881eb68cf6a90b780cb4b1
Jaroslav Jindrich <jajindri@microsoft.com>
2021-11-24 12:32:28 +0100

    vmzones na bicep

--- vmzones/deploy.ps1
+++ vmzones/deploy.ps1
@@ -1,4 +1,4 @@
-rg='jjzones-rg'
+$rg='jjzones-rg'
 
 az group create -n $rg -l westeurope
 az deployment group create -g $rg --template-file deploy-vmzones.bicep --parameters password=Azure-1234512345
