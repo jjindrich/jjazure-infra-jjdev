69b25442404d4af92751dde6684710e6f366c071
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2022-04-14 14:43:29 +0200

    appgw mtls

--- appgw/deploy.sh
+++ appgw/deploy.sh
@@ -2,4 +2,4 @@ rg='JJDevV2-Infra'
 
 # install appgw and nsg
 #az deployment group what-if -g $rg --template-file deploy-appgw.json --parameters deploy-appgw.params.json
-az deployment group create -g $rg --template-file deploy-appgw.json --parameters deploy-appgw.params.json
+az deployment group create -g $rg --template-file deploy-appgw-preview.json --parameters deploy-appgw-preview.params.json
