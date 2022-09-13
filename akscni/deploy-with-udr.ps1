559c8909fb28b0937f8826981275a08e27a0fa40
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2022-09-13 09:55:00 +0200

    aks cni

---
+++ akscni/deploy-with-udr.ps1
@@ -0,0 +1,20 @@
+# Deploy AKS private cluster with LoadBalancer used for outbound traffic
+
+$rg='JJDevV2-Infra'
+$rgmonitor='jjdevmanagement'
+$rgaks='jjmicroservices-rg'
+
+# Deploy AKS without public IP
+az aks create -n jjaksudr -g $rgaks `
+    -x -c 2 -z 1 2 3 `
+    --node-vm-size Standard_B2s `
+    --assign-identity $(az identity show -g $rgaks --n jjaksprivate --query id -o tsv) `
+    --network-plugin azure `
+    --vnet-subnet-id $(az network vnet subnet show --vnet-name JJDevV2NetworkApp -g $rg -n DmzAksPrivate --query id -o tsv) `
+    --enable-aad --enable-azure-rbac `
+    --enable-addons monitoring `
+    --workspace-resource-id $(az monitor log-analytics workspace show -g $rgmonitor -n jjdev-analytics --query id -o tsv) `
+    --outbound-type userDefinedRouting
+
+# Deploy application using intenal loadbalancer
+kubectl apply -f sample-private.yaml
