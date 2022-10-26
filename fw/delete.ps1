864feabfb88413b60ca258c0944222e7a41563bd
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2022-10-26 16:26:05 +0200

    fw dns

--- fw/delete.ps1
+++ fw/delete.ps1
@@ -1,6 +1,6 @@
 $rg='JJDevV2-Infra'
 
-# remove routes and delete firewall
+# remove routes, dns settings and delete firewall
 az network vnet subnet update -g $rg -n infra-snet --vnet-name jjazhubvnet --remove routeTable 
 az network vnet subnet update -g $rg -n GatewaySubnet --vnet-name jjazhubvnet --remove routeTable
 az network vnet subnet update -g $rg -n appgw-snet --vnet-name jjazhubvnet --remove routeTable
@@ -12,6 +12,8 @@ az network vnet subnet update -g $rg -n app-snet --vnet-name jjazappvnet --remov
 az network vnet subnet update -g $rg -n avd-snet --vnet-name jjazappvnet --remove routeTable
 az network vnet subnet update -g $rg -n function-snet --vnet-name jjazappvnet --remove routeTable
 az network vnet subnet update -g $rg -n ase-snet --vnet-name jjazappvnet --remove routeTable
+
+az network vnet update -g $rg -n jjazappvnet --dns-servers ''
 
 az resource delete -g $rg -n jjazfw --resource-type "Microsoft.Network/azureFirewalls"
 az resource delete -g $rg -n jjazfw-policy --resource-type "Microsoft.Network/firewallPolicies"
--- fw/deploy.ps1
+++ fw/deploy.ps1
@@ -1,8 +1,9 @@
 $rg ='jjnetwork-rg'
 
-# install firewall and add routes
+# install firewall
 az deployment group create -g $rg --template-file deploy-fw.bicep
 
+# assign routes to subnet
 az network vnet subnet update -g $rg -n infra-snet --vnet-name jjazhubvnet --route-table jjazhubvnet-infra-rt
 az network vnet subnet update -g $rg -n GatewaySubnet --vnet-name jjazhubvnet --route-table jjazhubvnet-vpn-rt
 az network vnet subnet update -g $rg -n appgw-snet --vnet-name jjazhubvnet --route-table jjazhubvnet-appgw-rt
@@ -14,5 +15,8 @@ az network vnet subnet update -g $rg -n app-snet --vnet-name jjazappvnet --route
 az network vnet subnet update -g $rg -n avd-snet --vnet-name jjazappvnet --route-table jjazappvnet-default-rt
 az network vnet subnet update -g $rg -n function-snet --vnet-name jjazappvnet --route-table jjazappvnet-default-rt
 az network vnet subnet update -g $rg -n ase-snet --vnet-name jjazappvnet --route-table jjazappvnet-default-rt
+
+# modify dns server on vnet to FW DNS proxy
+az network vnet update -g $rg -n jjazappvnet --dns-servers 10.3.252.4
 
 # add route into onprem
